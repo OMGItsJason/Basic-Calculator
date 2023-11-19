8fd18794a7db7bd54b6ea014346c24e296a5f5c0
Jason Chanliongco Maliquid <jasonm2822@gmail.com>
2023-11-19 20:25:19 +0800

    Add files via upload

---
+++ Basic Calculator/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using CalculatorPrivateAssembly;
+
+namespace Basic_Calculator
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            string[] _operators = new string[]
+            {
+                "+",
+                "-",
+                "*",
+                "/"
+            };
+            for (int o = 0; o < 4; o++)
+            {
+                CbOperators.Items.Add(_operators[o].ToString());
+            }
+        }
+
+        private void CalculateBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            string selectedOperator = CbOperators.SelectedItem as string;
+
+            float operandOne = float.Parse(OperandOne.Text);
+            float operandTwo = float.Parse(OperandTwo.Text);
+            float result = 0;
+
+            switch (selectedOperator)
+            {
+                case "+":
+                    result = BasicComputation.Addition(operandOne, operandTwo);
+                    break;
+                case "-":
+                    result = BasicComputation.Subtraction(operandOne, operandTwo);
+                    break;
+                case "*":
+                    result = BasicComputation.Multiplication(operandOne, operandTwo);
+                    break;
+                case "/":
+                    result = BasicComputation.Division(operandOne, operandTwo);
+                    break;
+                default:
+                    MessageBox.Show("Invalid");
+                    break;
+            }
+
+            Total.Text = result.ToString();
+        }
+    }
+}
